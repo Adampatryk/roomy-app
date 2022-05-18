7d2c47ca005d2ff27f35fd7b9174179bdac8a69c
Adam Kulpa <adamkulpa16@gmail.com>
2022-05-18 15:07:59 +0100

    Delete icon and max lenght

--- roomy/app/components/DeleteIcon.js
+++ roomy/app/components/DeleteIcon.js
@@ -1,9 +1,15 @@
 import { MaterialIcons } from "@expo/vector-icons";
 import colors from "../config/colors";
 
-const DeleteIcon = ({ size = 28, color = colors.BLACK, onPress }) => {
+const DeleteIcon = ({ size = 28, color = colors.BLACK, onPress, style }) => {
   return (
-    <MaterialIcons onPress={onPress} name="cancel" size={size} color={color} />
+    <MaterialIcons
+      style={style}
+      onPress={onPress}
+      name="cancel"
+      size={size}
+      color={color}
+    />
   );
 };
 
--- roomy/app/components/lists/EditableListItem.js
+++ roomy/app/components/lists/EditableListItem.js
@@ -1,6 +1,13 @@
 import { useIsFocused, useNavigation } from "@react-navigation/native";
 import React, { useRef, useState } from "react";
-import { Button, Keyboard, StyleSheet, Text, TextInput } from "react-native";
+import {
+  Button,
+  Keyboard,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 import colors from "../../config/colors";
 import DeleteIcon from "../DeleteIcon";
 import AppTextInput from "../AppTextInput";
@@ -9,35 +16,66 @@ import ListItemContainer from "./ListItemContainer";
 import globalStyles from "./styles";
 
 const EditableListItem = ({ title, saveTitle, onDelete }) => {
+  const [editMode, setEditMode] = useState(false);
   const [value, setValue] = useState(title);
   const inputRef = useRef();
 
   const style = {
     ...globalStyles.title,
+    ...styles.input,
+  };
+
+  const stopEditting = () => {
+    saveTitle(value);
+    setEditMode(false);
   };
 
   return (
     <ListItemContainer
       onPress={() => {
+        setEditMode(true);
         inputRef.current.focus();
       }}
     >
-      <TextInput
-        ref={inputRef}
-        style={style}
-        value={value}
-        onChangeText={(value) => setValue(value)}
-        onSubmitEditing={() => saveTitle(value)}
-      />
+      <View style={styles.container}>
+        <TextInput
+          ref={inputRef}
+          style={style}
+          value={value}
+          onChangeText={(value) => setValue(value)}
+          onSubmitEditing={stopEditting}
+          onFocus={() => setEditMode(true)}
+          onBlur={() => stopEditting()}
+          maxLength={25}
+        />
 
-      <DeleteIcon onPress={onDelete} color={colors.WHITE} />
+        {editMode && (
+          <DeleteIcon
+            onPress={onDelete}
+            color={colors.WHITE}
+            size={22}
+            style={styles.deleteIcon}
+          />
+        )}
+      </View>
     </ListItemContainer>
   );
 };
 
 const styles = StyleSheet.create({
   input: {
-    color: colors.SECONDARY,
+    width: "100%",
+    textAlign: "center",
+  },
+  container: {
+    flexDirection: "row",
+    width: "100%",
+  },
+  deleteIcon: {
+    padding: 0,
+    margin: 0,
+    position: "absolute",
+    right: 0,
   },
 });
 
